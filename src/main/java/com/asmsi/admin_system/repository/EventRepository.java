71a656f273a762dcb9a69fa5ce4be50090b63d85
JPENASO <84899665+jpenaso10@users.noreply.github.com>
2025-04-05 14:24:45 +0800

    update/event-page-school-year

--- src/main/java/com/asmsi/admin_system/repository/EventRepository.java
+++ src/main/java/com/asmsi/admin_system/repository/EventRepository.java
@@ -2,8 +2,13 @@ package com.asmsi.admin_system.repository;
 
 import com.asmsi.admin_system.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+    @Query("SELECT e FROM Event e WHERE YEAR(e.dateTime) = :year")
+    List<Event> findByYear(@Param("year") int year);
 }
