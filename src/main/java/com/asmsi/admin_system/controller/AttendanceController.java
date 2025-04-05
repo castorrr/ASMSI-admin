beeb3f5388efb4d4caf2bc9d41c6dc6e1ff26147
Jay28Ladera <149279350+Jay28Ladera@users.noreply.github.com>
2025-04-05 14:16:47 +0800

    feature/studentmanager

---
+++ src/main/java/com/asmsi/admin_system/controller/AttendanceController.java
@@ -0,0 +1,14 @@
+package com.asmsi.admin_system.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AttendanceController {
+
+    @GetMapping("/attendance")
+    public String showAttendancePage() {
+        // Return the name of the HTML page to render
+        return "attendance"; // This will render attendance.html
+    }
+}
