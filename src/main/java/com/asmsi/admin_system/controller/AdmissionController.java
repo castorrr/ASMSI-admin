f7f7f706ec73e6d40030665abcd5eb617e06e6f6
Rhey Alvin Quilapio <rheyalvin.quilapio@cit.edu>
2025-04-05 12:12:19 +0800

    ad
    asdas

--- src/main/java/com/asmsi/admin_system/controller/AdmissionController.java
+++ src/main/java/com/asmsi/admin_system/controller/AdmissionController.java
@@ -3,12 +3,18 @@ package com.asmsi.admin_system.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import com.asmsi.admin_system.service.AuthUtil;
 
 @Controller
 public class AdmissionController {
 
+    private final AuthUtil authUtil = new AuthUtil();
+
     @GetMapping("/admission")
     public String admissionPage(Model model) {
+       
+        model.addAttribute("username", authUtil.getCurrentUser().getUsername());
+        model.addAttribute("role", authUtil.getCurrentUser().getRole());
         return "admission"; // Renders src/main/resources/templates/admission.html
     }
 }
