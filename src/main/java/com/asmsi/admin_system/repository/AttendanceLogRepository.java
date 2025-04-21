3cd1b209e6cfe47ba0906ccadaaeccdd2e176788
Jay28Ladera <149279350+Jay28Ladera@users.noreply.github.com>
2025-04-21 15:51:31 +0800

    attendancelogsinitial

--- src/main/java/com/asmsi/admin_system/repository/AttendanceLogRepository.java
+++ src/main/java/com/asmsi/admin_system/repository/AttendanceLogRepository.java
@@ -3,10 +3,15 @@ package com.asmsi.admin_system.repository;
 import com.asmsi.admin_system.entity.AttendanceLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
+
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface AttendanceLogRepository extends JpaRepository<AttendanceLog, Long> {
     List<AttendanceLog> findByIdNumber(String idNumber);
+    Optional<AttendanceLog> findTopByIdNumberAndDateOrderByTimeInDesc(String idNumber, LocalDate date);
+
 }
