706e216201ef9fb483d8fd7aabca7ac79c7c5d01
JPENASO <84899665+jpenaso10@users.noreply.github.com>
2025-04-26 11:27:49 +0800

    update

--- src/main/java/com/asmsi/admin_system/repository/EventAttendanceRepository.java
+++ src/main/java/com/asmsi/admin_system/repository/EventAttendanceRepository.java
@@ -2,6 +2,8 @@ package com.asmsi.admin_system.repository;
 
 import com.asmsi.admin_system.entity.EventAttendance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface EventAttendanceRepository extends JpaRepository<EventAttendance, Long> {
+    List<EventAttendance> findByStudentId(String studentId);
 }
